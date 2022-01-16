a2e558064094f56052590255165770716dd06d79
Iverson Leitzke <iversonleitzke@gmail.com>
2022-01-16 18:22:59 -0300

    Drying code

--- Projects/First project/script.js
+++ Projects/First project/script.js
@@ -4,14 +4,17 @@ let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highScore = Number(document.querySelector('.highscore').textContent);
 
+const displatMsg = function (msg) {
+    document.querySelector('.message').textContent = msg;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     if (score > 0) {
         if (!guess) {
-            document.querySelector('.message').textContent = '⛔ No number!';
+            displatMsg('⛔ No number!');
         } else if (secretNumber == guess) {
-            document.querySelector('.message').textContent =
-                '🏆 Correct Number';
+            displatMsg('🏆 Correct Number');
             if (score > highScore) {
                 document.querySelector('.highscore').textContent = score;
                 highScore = score;
@@ -19,26 +22,21 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.number').textContent = secretNumber;
             document.querySelector('body').style.backgroundColor = '#60b347';
             document.querySelector('.number').style.width = '30rem';
-        } else if (secretNumber < guess) {
-            document.querySelector('.message').textContent = '📈 Too high';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else if (secretNumber > guess) {
-            document.querySelector('.message').textContent = '📉 Too low';
+        } else if (secretNumber != guess) {
+            displatMsg(guess > secretNumber ? '📈 Too high' : '📉 Too low');
             score--;
             document.querySelector('.score').textContent = score;
         }
-        if (score < 1) {
-            document.querySelector('.message').textContent =
-                '🎆You missed the game!';
-        }
+    }
+    if (score < 1) {
+        displatMsg('🎆You missed the game!');
     }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20 + 1);
-    document.querySelector('.message').textContent = 'Start guessing...';
+    displatMsg('Start guessing...');
     document.querySelector('.number').textContent = '?';
     document.querySelector('.score').textContent = '20';
     document.querySelector('.guess').value = '';
