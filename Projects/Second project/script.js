f3399d5f0a9bae1bba9bef7d445cbf81212b03d4
Iverson Leitzke <iversonleitzke@gmail.com>
2022-01-18 20:40:54 -0300

    Finished project 2

--- Projects/Second project/script.js
+++ Projects/Second project/script.js
@@ -20,5 +20,9 @@ btnsOpenModal.forEach((element, key) => {
 });
 
 btn.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden'))
+        closeModal();
+});
