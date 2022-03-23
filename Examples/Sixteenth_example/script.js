ce566b836b5ce890529b44bfad97e8a00f656d72
IversonLeitzke <iversonleitzke@gmail.com>
2022-03-23 19:56:22 -0300

    Strings commit two

--- Examples/Sixteenth_example/script.js
+++ Examples/Sixteenth_example/script.js
@@ -10,3 +10,24 @@ console.log(airline.indexOf('r'));
 console.log(airline.lastIndexOf('i'));
 
 console.log(airline.slice(4));
+
+console.log(airline.toLowerCase());
+
+console.log(airline.toUpperCase());
+
+console.log(' Iverson'.toLowerCase().trim());
+
+console.log('288.25'.replace('.', ','));
+
+const announcement =
+    'All passengers come to boarding door 23. Boarding door 23.';
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+const plane2 = 'A320neo';
+
+console.log(plane2.includes('320'));
+
+console.log(plane2.startsWith('A420'));
+
+console.log(plane2.endsWith('neo'));
