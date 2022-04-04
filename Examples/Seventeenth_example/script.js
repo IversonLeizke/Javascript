8d1e70298fafbfaea11277d9c784c58236b26b90
IversonLeitzke <iversonleitzke@gmail.com>
2022-04-04 10:46:49 -0300

    High order functions

--- Examples/Seventeenth_example/script.js
+++ Examples/Seventeenth_example/script.js
@@ -13,3 +13,24 @@ const createBoking = function (flightNum, numPassengers = 60, price = 1000) {
 createBoking('LH123', 52, 1500);
 createBoking('Lh246');
 createBoking('Lh246', undefined, 1300);
+
+// CallBack Function
+
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
