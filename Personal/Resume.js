8f435cd12338dcda2f58094f537540af51a1f4cc
IversonLeitzke <iversonleitzke@gmail.com>
2022-04-12 13:07:50 -0300

    Resume

---
+++ Personal/Resume.js
@@ -0,0 +1,28 @@
+const resume = {
+    personal: {
+        name: 'Iverson Leitzke',
+        birthDay: '1989/17/11',
+        age: 32,
+        gender: 'Male',
+        maritalStatus: 'Single',
+        driversLicense: 'AD',
+        phone: '47-9####-####',
+        email: ['iversonleitzke@gmail.com', 'iversonleitzke@protonmail.com'],
+        nationality: 'Brasileiro',
+        adress: new Map({
+            street: 'Rua Aldano José Vieira',
+            number: 712,
+            neighborhood: 'Avai',
+            city: 'Guaramirim',
+            state: 'SC',
+        }),
+    },
+    jobExperience: [
+        {
+            company: 'Zanotti S.A',
+            position: 'Analista Financeiro',
+            from: '09/2021',
+            to: '02/2022',
+        },
+    ],
+};
